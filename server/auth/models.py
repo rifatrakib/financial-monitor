6d7591eb4825fa02d1a1bc177ce8f4ed79704d5f
rifatrakib <Rakib.1508@outlook.com>
2022-10-07 12:22:11 +0600

    user model  for database table creation and migration

---
+++ server/auth/models.py
@@ -0,0 +1,50 @@
+import re
+from datetime import datetime
+
+from email_validator import EmailNotValidError, validate_email
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy.orm import validates
+
+from server.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    middle_name = Column(String, default=None)
+    last_name = Column(String, default=None)
+    email_address = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    phone_number = Column(String, unique=True, index=True)
+    gender = Column(String, default=None)
+    birthday = Column(DateTime(timezone=True), default=None)
+    is_active = Column(Boolean, default=True)
+
+    @validates("email_address")
+    def email_validator(self, key, address):
+        try:
+            val = validate_email(address)
+            email = val["email"]
+            return email
+        except EmailNotValidError as e:
+            print(str(e))
+            print(f"provide a valid {key}")
+
+    @validates("phone_number")
+    def phone_number_validator(self, key, number):
+        regex = r"^([0-9\(\)\/\+ \-]*)$"
+        pat = re.compile(regex)
+        if number and not re.search(pat, number):
+            raise ValueError(f"phone number invalid. provide a valid {key}")
+
+        return number
+
+    @validates("birthday")
+    def birthday_validator(self, key, dt):
+        if dt > datetime.utcnow():
+            raise ValueError(f"birthday cannot be a future date. provide a valid {key}")
+
+        return dt
