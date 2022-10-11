b3911937cf33c335fde829fc8200f3a6bbd2a95b
rifatrakib <Rakib.1508@outlook.com>
2022-10-11 15:25:27 +0600

    modify and add crud functions

--- server/auth/crud.py
+++ server/auth/crud.py
@@ -1,4 +1,4 @@
-from sqlalchemy import insert, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import models, schemas
@@ -7,10 +7,9 @@ from . import models, schemas
 async def create_new_user(session: AsyncSession, user: schemas.UserCreate):
     data = user.dict()
     data["gender"] = data["gender"].value
-    query = insert(models.User).values(data).returning(models.User.id)
-    user_id = await session.execute(query)
+    user_data = models.User(**data)
+    session.add(user_data)
     await session.commit()
-    user_data = {**data, "id": user_id.scalars().first()}
     return user_data
 
 
@@ -18,4 +17,18 @@ async def get_user_by_id(session: AsyncSession, user_id: int):
     query = select(models.User).filter(models.User.id == user_id)
     user_data = await session.execute(query)
     user = user_data.scalars().first()
+    return user
+
+
+async def get_user_by_username(session: AsyncSession, username: str):
+    query = select(models.User).filter(models.User.username == username)
+    user_data = await session.execute(query)
+    user = user_data.scalars().first().__dict__
+    return user
+
+
+async def get_user_by_email(session: AsyncSession, email_address: str):
+    query = select(models.User).filter(models.User.email_address == email_address)
+    user_data = await session.execute(query)
+    user = user_data.scalars().first().__dict__
     return user
