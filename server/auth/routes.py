fe4e861fa3680b79572facbbe2fd9286b4014c78
rifatrakib <Rakib.1508@outlook.com>
2022-10-13 13:00:25 +0600

    add endpoints for register and login with OAuth2.0

--- server/auth/routes.py
+++ server/auth/routes.py
@@ -1,28 +1,52 @@
-from fastapi import APIRouter, Depends
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from ..database import get_database_session
+from ..config import read_config
+from ..database import get_session
 from .crud import create_new_user, get_user_by_id
-from .schemas import User, UserCreate
+from .dependencies import get_current_active_user
+from .schemas import AuthToken, Password, User, UserCreate, UserInRequest, UserInResponse
+from .utils import authenticate_user, create_access_token, get_password_hash
 
 router = APIRouter(prefix="/auth")
 
 
-async def get_session():
-    try:
-        session: AsyncSession = get_database_session()()
-        yield session
-    finally:
-        await session.close()
+@router.post("/register/", response_model=UserInResponse)
+async def register_new_user(user: UserInRequest, session: AsyncSession = Depends(get_session)):
+    validated_password = Password(password=user.password)
+    hashed_password = get_password_hash(validated_password.password)
+    user_data = UserCreate(**user.dict(), hashed_password=hashed_password)
+    created_user = await create_new_user(session, user_data)
+    return created_user
 
 
-@router.post("/users/", response_model=User)
-async def create_user(user: UserCreate, session: AsyncSession = Depends(get_session)):
-    user = await create_new_user(session, user)
+@router.post("/login/", response_model=AuthToken)
+async def login_for_access_token(
+    session: AsyncSession = Depends(get_session), form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = await authenticate_user(session, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=read_config("access_token_expiry"))
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/me/", response_model=UserInResponse)
+async def read_current_user(
+    user: User = Depends(get_current_active_user), session: AsyncSession = Depends(get_session)
+):
     return user
 
 
-@router.get("/users/", response_model=User)
+@router.get("/users/", response_model=UserInResponse, dependencies=[Depends(get_current_active_user)])
 async def read_user(user_id: int, session: AsyncSession = Depends(get_session)):
     user = await get_user_by_id(session, user_id)
     return user
